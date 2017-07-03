3fe355cb63daf8292a863b0b2ff8ef33e0b80bef
lyl <757365800@qq.com>
2017-07-03 23:01:32 +0800

    第二次提交（包含MVCWeb）

---
+++ DotnetcoreMVC20.Web/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetcoreMVC20.Web.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
